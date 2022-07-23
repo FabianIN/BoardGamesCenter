5236cff82ce2fc3f7ecc69cce9fc1929957d6ff3
Fabian Neagoe <Ionut.neagoe@student.unitbv.ro>
2022-07-23 13:53:10 +0300

    Changes 7.23.22

---
+++ Contexts/GamesContext.cs
@@ -0,0 +1,22 @@
+﻿using BoardGamesCenter.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BoardGamesCenter.Contexts
+{
+    public class GamesContext : DbContext
+    {
+        public GamesContext(DbContextOptions<GamesContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+
+        public DbSet<Game> Games { get; set; }
+
+        public DbSet<Publisher> Publishers { get; set; }
+     }
+}
---
+++ Entities/Publisher.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BoardGamesCenter.Entities
+{    public class Publisher
+    {
+        [Key]
+
+        public Guid ID { get; set; }
+        [Required]
+        [MaxLength(150)]
+        public string CompanyName { get; set; }
+
+        [MaxLength(2500)]
+        public string CompanyDescription { get; set; }
+
+        public bool? Deleted { get; set; }
+    }
+}
+}
